5dea5d1fa048f6871764599bf966f50d6ac96d2b
gana2003 <meenakshigana0@gmail.com>
2022-07-08 12:34:06 +0530

    shoppingapp cmpltd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingcartComponent
   ],
   imports: [
     BrowserModule,
